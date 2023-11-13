91c87db7299d6fb20eba42a1e3749859b1f021fb
hjenp22 <hjipark22@gmail.com>
2023-11-13 15:58:28 -0600

    made it work need to fine tooth

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,50 +1,75 @@
-// Initialize and add the map
-let map;
+// // Initialize and add the map
 
-async function initMap() {
-  // The location of Uluru
-  const position = { lat: 41.881832, lng: -87.623177 };
-  // Request needed libraries.
-  //@ts-ignore
-  const { Map } = await google.maps.importLibrary("maps");
-  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+// let map;
 
-  // The map, centered at Uluru
-  map = new Map(document.getElementById("map"), {
-    zoom: 10,
-    center: position,
-    mapId: "DEMO_MAP_ID",
-  });
+// async function initMap() {
+//   // The location of Uluru
 
-  // The marker, positioned at Uluru
-  const marker = new AdvancedMarkerElement({
-    map: map,
-    position: position,
-    title: "Uluru",
-  });
-}
-initMap();
+//   const position = { lat: 41.881832, lng: -87.623177 };
 
+//   // Request needed libraries.
+//   //@ts-ignore
 
-const apiKey = "73b0cba1ba046852f98801a447debff3"; //open weather
-const city = 'destination'
+//   const city = 'destination'
+//   const { Map } = await google.maps.importLibrary("maps");
+//   const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
 
-const apiUrl = `https:api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+//   // The map, centered at Uluru
+//   map = new Map(document.getElementById("map"), {
+//     zoom: 10,
+//     center: position,
+//     mapId: "DEMO_MAP_ID",
+//   });
 
-// https://api.openweathermap.org/geo/1.0/direct?q=orlando&limit=1&appid=486a30fe2a4040f404391459060015ad
-
-// var requestLocationUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${searchInput}&limit=1&appid=${apiKey}`
-//^ link to feed our data to get log/lat need to fetch
+//   // The marker, positioned at Uluru
+//   const marker = new AdvancedMarkerElement({
+//     map: map,
+//     position: position,
+//     title: "Uluru",
+//   });
+// }
+// initMap();
 
 
+// const apiKey = "73b0cba1ba046852f98801a447debff3"; //open weather
+// const city = 'destination'
 
-// $("#city").on("change keyup", function() {
-//     var city = $(this).val()
-//     $.getJSON("https://maps.googleapis.com/maps/api/geocode/json?address="+encodeURIComponent(city), function(val) {
-//       if(val.results.length) {
-//         var location = val.results[0].geometry.location
-//         $("#lat").val(location.lat)
-//         $("#lon").val(location.lng)
-//       }
+// const apiUrl = `https:api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+// // https://api.openweathermap.org/geo/1.0/direct?q=orlando&limit=1&appid=486a30fe2a4040f404391459060015ad
+
+// // var requestLocationUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${searchInput}&limit=1&appid=${apiKey}`
+// //^ link to feed our data to get log/lat need to fetch
+
+
+
+// // $("#city").on("change keyup", function() {
+// //     var city = $(this).val()
+// //     $.getJSON("https://maps.googleapis.com/maps/api/geocode/json?address="+encodeURIComponent(city), function(val) {
+// //       if(val.results.length) {
+// //         var location = val.results[0].geometry.location
+// //         $("#lat").val(location.lat)
+// //         $("#lon").val(location.lng)
+// //       }
+// //     })
+// //   })
+
+
+
+
+
+// const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+// fetch(weatherUrl)
+//     .then(response => response.json())
+//     .then(weatherData => {
+
 //     })
-//   })
+//     .catch(error => console.error('Error fetching weather data:', error));
+
+// const currencyUrl = `${baseUrl}convert?from=USD&to=EUR&quantity=${weatherData.main.temp}&apikey=${currencyApiKey}`;
+// fetch(currencyUrl)
+//      .then(response => response.json())
+//      .then(currencyData => {
+//      })
+//      .catch(error => console.error('Error fetching currency data:', error));
