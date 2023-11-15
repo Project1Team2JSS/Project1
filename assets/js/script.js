5400d12cd5348070a961735ed62f1a7237d89ecc
Sean Harrigan <SeanHarrigan8@gmail.com>
2023-11-14 19:53:35 -0600

    newConvertCountryAPIS BUHBUHBUHBOOM

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,85 +1,44 @@
 console.log("connected");
 var currencyArray;
 
-//designates destination value//
-// var destination = document.getElementById('destination').value;
+var countryApi = "C4ZcT3i97dTmhMQh9pLU6Q==5lj4TfH531vJD2Tv";
 
-//designate budget value//
-var amount = document.getElementById("budget").value;
+const getCountryCurrency = async (countryName, amount) => {
+  console.log(amount);
+  const data = await fetch(
+    `https://api.api-ninjas.com/v1/country?name=${countryName}`,
+    {
+      method: "GET",
+      headers: {
+        "X-Api-Key": countryApi,
+        "Content-Type": "application/json",
+      },
+    }
+  );
+  const jsonData = await data.json();
+  const currencyCode = jsonData[0].currency.code;
+  console.log(currencyCode);
 
-// API key
-var key = "fca_live_0OUoo47t39YgiyRkWn6hAtnHSkBcNBPguWidPeWQ";
-// Fetch call to grab currencies
-fetch(`https://api.freecurrencyapi.com/v1/latest?apikey=${key}&currencies=`)
-  .then((response) => response.json())
-  .then((currencyData) => {
-    console.log(currencyData);
-    currencyArray = currencyData.data;
-  });
-
-//https://api.freecurrencyapi.com/v1/latest?apikey=fca_live_0OUoo47t39YgiyRkWn6hAtnHSkBcNBPguWidPeWQ&currencies=e
-
-
-
-function getCountryCurrency(countryName) {
-  console.log("made it here");
-
-  fetch(`https://restcountries.eu/rest/v2/name/${countryName}`)
-  
-
-    .then((response) => response.json())
-    .then((data) => {
-      // Check if country was found
-      if (data[0]) {
-        // Get the currency code, name, and symbol
-        const currencyCode = data[0].currencies[0].code;
-        const currencyName = data[0].currencies[0].name;
-        const currencySymbol = data[0].currencies[0].symbol;
-
-        console.log(`Country: ${countryName}`);
-        console.log(`Currency Code: ${currencyCode}`);
-        console.log(`Currency Name: ${currencyName}`);
-        console.log(`Currency Symbol: ${currencySymbol}`);
-      } else {
-        console.log(`No data found for ${countryName}`);
-      }
-    })
-    .catch((error) => console.error("Error:", error));
-}
-
-// Call the function with a country name
-// getCountryCurrency('United States');
-
-// document.addEventListener('DOMContentLoaded', getCountryCurrency())
+  const currencyConvertapi = await fetch(
+    `https://api.api-ninjas.com/v1/convertcurrency?have=USD&want=${currencyCode}&amount=${amount}`,
+    {
+      method: "GET",
+      headers: {
+        "X-Api-Key": countryApi,
+        "Content-Type": "application/json",
+      },
+    }
+  );
+  const jsoncurrencyConvertapi = await currencyConvertapi.json();
+  console.log(jsoncurrencyConvertapi);
+  // console.log(jsoncurrencyConvertapi.new_amount)
+};
 
 var budgetButton = document.getElementById("tip");
 // adding click listenter
 budgetButton.addEventListener("click", function (event) {
   event.preventDefault();
-
   var countryName = document.getElementById("destination").value;
-  getCountryCurrency(countryName);
+  var amount = document.getElementById("budget").value;
+  getCountryCurrency(countryName, amount);
 });
-
-//make api call to acquire this info//
-//convert entered amount to destinnation currency
-
-// //OUTPUT OF ABOVE LOOKS LIKE THIS://
-// // //Country: United States
-// // Currency Code: USD
-// // Currency Name: United States dollar
-// // Currency Symbol: $
-
-// country: Ireland
-// code: EUR
-// curr name: euro 
-// curr symbol: symbol
-
-
-// //event listener on = button where user inputs data , whatever country then call function when they hit submit on said form//
-// //destination  maybe move your budget feature//
-
-// //budget
-// //destination
-// //NEW PAGE//
-// //new tabe
