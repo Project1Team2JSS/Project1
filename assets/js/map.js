3a48c295d070c75397a07504d894374a5f88edad
hjenp22 <hjipark22@gmail.com>
2023-11-15 17:50:47 -0600

    map to js

--- assets/js/map.js
+++ assets/js/map.js
@@ -116,3 +116,45 @@
 //      .then(currencyData => {
 //      })
 //      .catch(error => console.error('Error fetching currency data:', error));
+
+
+function initMap() {
+    // Initialize the map
+    const { AdvancedMarkerElement } = google.maps.importLibrary("marker");
+    const map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 8
+    });
+
+    // Initialize the geocoder
+    const geocoder = new google.maps.Geocoder();
+
+    // Store the marker globally
+    window.marker = new google.maps.Marker({ map: map });
+}
+
+function getCoordinates() {
+    const location = document.getElementById('locationInput').value;
+
+    if (location.trim() === '') {
+        alert('Please enter a location.');
+        return;
+    }
+
+    const geocoder = new google.maps.Geocoder();
+
+    geocoder.geocode({ address: location }, (results, status) => {
+        if (status === 'OK') {
+            const locationData = results[0].geometry.location;
+            const map = new google.maps.Map(document.getElementById('map'), {
+                center: locationData,
+                zoom: 12
+            });
+
+            // Set marker position
+            window.marker.setPosition(locationData);
+        } else {
+            alert('Geocoding was not successful for the following reason: ' + status);
+        }
+    });
+}
